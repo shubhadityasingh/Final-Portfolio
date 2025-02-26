899300b6b56fb7e9b14dfadc76c76cde9f1c5dba
Shubhaditya <shubh28062000@gmail.com>
2025-02-27 03:32:51 +0530

    background blur and navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { LINKS } from "../constants";
+import { RiCloseFill, RiMenu3Fill } from "@remixicon/react";
+import { div } from "framer-motion/client";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -26,7 +28,36 @@ const Navbar = () => {
             </a>
           ))}
         </div>
+
+        <div className="md:hidden">
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="text-white focus:outline-none"
+            aria-label={menuOpen ? "Close menu" : "Open menu"}
+          >
+            {menuOpen ? (
+              <RiCloseFill className="w-6 h-6" />
+            ) : (
+              <RiMenu3Fill className="w-6 h-6" />
+            )}
+          </button>
+        </div>
       </div>
+
+      {menuOpen && (
+        <div className="md:hidden p-2 bg-stone-950/30 backdrop-blur-lg rounded-xl flex flex-col space-y-4 max-w-6xl mx-auto">
+          {LINKS.map((link, index) => (
+            <a
+              href={link.href}
+              key={index}
+              className="text-white hover:text-stone-400 transition duration-300"
+              onClick={handleClickLink}
+            >
+              {link.label}
+            </a>
+          ))}
+        </div>
+      )}
     </nav>
   );
 };
